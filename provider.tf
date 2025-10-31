4672782c4850e0f9cd3237e685d8eb790915265e
daniel-alves-swo <daniel.alves@softwareone.com>
2025-10-31 13:46:36 -0300

    provider error correction

--- provider.tf
+++ provider.tf
@@ -1,22 +1,6 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.4.0"
-    }
-  }
-  # No backend block aqui: o Terraform Cloud vai gerenciar o state pelo workspace VCS.
-}
-
 provider "aws" {
   region = var.region
 
-  # Opcional: tags padrão
   default_tags {
     tags = {
       Project   = var.project_name
